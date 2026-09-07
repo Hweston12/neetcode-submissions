b44030c3b482852926f26c9627a6ebd83940d4fe
Hweston12 <90007389+Hweston12@users.noreply.github.com>
2026-09-07 13:03:29 -0400

    Add: squares-of-a-sorted-array - submission-3

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1; 
+        int[] res = new int[nums.length];
+        int idx = r; 
+
+        while(l <= r){
+            if(nums[l] * nums[l] > nums[r] * nums[r]){
+                res[idx--] = nums[l] * nums[l];
+                l++;
+            }else{
+                res[idx--] = nums[r] * nums[r];
+                r--; 
+            }
+        }
+        return res; 
+    }
+}
