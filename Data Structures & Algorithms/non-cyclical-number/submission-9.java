8443d62d992694bdf477f02e40c84d69a2d430da
Hweston12 <90007389+Hweston12@users.noreply.github.com>
2026-09-16 16:17:29 -0400

    Add: non-cyclical-number - submission-9

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while(!seen.contains(n)){
+            seen.add(n);
+            n = sumOfSquares(n);
+            if(n == 1){
+                return true; 
+            }
+        }
+        return false; 
+    }
+
+    private int sumOfSquares(int n){
+        int output = 0;
+
+        while(n > 0){
+            int digit = n % 10;
+            digit = digit * digit; 
+            output += digit;
+            n /= 10; 
+        }
+        return output; 
+    }
+}
+
